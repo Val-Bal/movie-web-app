69125f390131d687018b261e649365dd5cd0dbe1
Valeria Balducci <v_balducci@web.de>
2023-03-10 12:37:55 +0100

    feat: add HTTP requests and responses

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//
 const express = require('express'),
 morgan = require('morgan'),
  app = express(),
@@ -6,6 +7,7 @@ morgan = require('morgan'),
 
  app.use(bodyParser.json());
 
+//
 app.use(express.static('public'));
 app.use(morgan('common'));
 
@@ -155,6 +157,124 @@ let movies = [
     }
   ];
 
+///////       READ = get      ///////
+app.get('/movies', (req, res) => {
+  res.status(200).json(movies);
+})
+
+// READ
+app.get('/movies:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movies.Title === title );
+
+  if (movie) {
+    return res.status(200).json(movie);
+  } else {
+    res.status(400).send('no such movie')
+  }
+
+})
+
+// READ
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movies.Genre === genreName ).Genre;
+
+  if (genre) {
+    return res.status(200).json(genre);
+  } else {
+    res.status(400).send('no such genre')
+  }
+
+})
+
+// READ 
+app.get('/movies/directors/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movies.Director.Name === directorName ).Director;
+
+  if (director) {
+    return res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director')
+  }
+
+})
+
+///////       CREATE = post      ///////
+// CREATE 
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send('users need names')
+  }
+})
+
+// CREATE 
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send('no such movie')
+  }
+})
+
+ ///////       UPDATE = put      ///////
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('user not found')
+  }
+})
+
+
+ ///////       DELETE = delete      ///////
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle )
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send('no such movie')
+  }
+})
+
+
+// DELETE 
+app.post('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    users = user.filter( user => user.id != id )
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+
 // GET requests documentation
 app.get('/', (req, res) => {
   res.send('Welcome to Indiflix!');
@@ -165,7 +285,7 @@ app.get('/documentation', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
 
 // GET requests index --> does not work yet
