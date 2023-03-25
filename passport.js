d501b6f9e6252eac808232cb4fe634a31b876dee
Valeria Balducci <v_balducci@web.de>
2023-03-25 16:56:29 +0100

    fix: bug fix local strategy

--- passport.js
+++ passport.js
@@ -7,26 +7,35 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(new LocalStrategy({
-  usernameField: 'Username',
-  passwordField: 'Password'
-}, (username, password, callback) => {
-  console.log(username + '  ' + password);
-  Users.findOne({ Username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
-    }
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: "Username",
+            passwordField: "Password",
+        },
+        async (username, password, callback) => {
+            console.log(username + "  " + password);
+            const user = await Users.findOne({ Username: username }).catch(
+                (error) => {
+                    if (error) {
+                        console.log(error);
+                        return callback(error);
+                    }
+                }
+            );
 
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
-    }
+            if (!user) {
+                console.log("incorrect username");
+                return callback(null, false, {
+                    message: "Incorrect username or password.",
+                });
+            }
 
-    console.log('finished');
-    return callback(null, user);
-  });
-}));
+            console.log("finished");
+            return callback(null, user);
+        }
+    )
+);
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
