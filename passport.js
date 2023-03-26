7733ddb1b352d7d5defcc932ddd6b192369c9b9f
Valeria Balducci <v_balducci@web.de>
2023-03-26 04:44:15 +0200

    Update passport.js

--- passport.js
+++ passport.js
@@ -25,11 +25,14 @@ passport.use(
             );
 
             if (!user) {
-                console.log("incorrect username");
-                return callback(null, false, {
-                    message: "Incorrect username or password.",
-                });
-            }
+                console.log('incorrect username');
+                return callback(null, false, {message: 'Incorrect username.'});
+              }
+
+            if (!user.validatePassword(password)) {
+                console.log('incorrect password');
+                return callback(null, false, {message: 'Incorrect password.'});
+              }
 
             console.log("finished");
             return callback(null, user);
